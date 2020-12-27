dd864fff719a54ec31d7a6e1f38afcc7abefe9a1
Aleksandr Bolokhov <bolohov@inbox.ru>
2020-12-27 17:39:59 +0300

    rework

--- src/components/Messege.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Messege = ({text}) => {
-    return <span class="text">{text}</span>;
-};
-
-export default Messege;
--- src/helper.js
+++
@@ -1,7 +0,0 @@
-/*
-const practice1 = () => console.log("result1");
-export function practice2 () {
-       console.log("result2");
-};
-export default practice1;
-*/
