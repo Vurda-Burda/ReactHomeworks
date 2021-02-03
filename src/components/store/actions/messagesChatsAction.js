843747fd7da5814bbc155727433018ed7931b291
Aleksandr Bolokhov <bolohov@inbox.ru>
2021-02-03 12:46:04 +0300

    plug

--- src/components/store/actions/messagesChatsAction.js
+++
@@ -1,8 +0,0 @@
-export const MESSAGES_CHATS = "MESSAGES_CHATS";
-
-export const messagesChats = (message, author, chatId) => ({
-    type: MESSAGES_CHATS,
-    message,
-    author,
-    chatId
-});
--- src/components/store/actions/testActions.js
+++
@@ -1,6 +0,0 @@
-export const TEST_TEXT = "TEST_TEXT";
-
-export const testText = (text) => ({
-    type: TEST_TEXT,
-    text,
-});
