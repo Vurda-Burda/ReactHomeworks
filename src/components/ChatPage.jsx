84a175c89e3118b5425ce6744830653cc02a7825
Aleksandr Bolokhov <bolohov@inbox.ru>
2021-02-04 16:34:17 +0300

    begin homework-7

---
+++ src/components/ChatPage.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import MessageList from "./MessageList";
+import SendMessage from "./SendMessage";
+
+import {connect} from "react-redux";
+import {messagesChats} from "./store/actions/messagesChatsAction";
+
+
+class ChatPage extends React.Component {
+    state = {
+        messages: [],
+        infoТext: "no chat selected"
+    }
+
+
+    static propTypes = {
+        chatId: PropTypes.number
+    }
+
+    static defaultProps = {
+        chatId: 0
+    }
+
+    chatSelected = () => {
+        if(this.props.chatId === 0) {
+             return this.state.infoТext;
+        } else {
+            return this.props.chatId;
+        };
+    }
+
+    send = objMsg => {
+        this.setState({messagesChatsStore:  this.props.messagesChatsStore.push(objMsg)}, () => this.setState( () => this.props.messagesChatsStore.push({message: 'I do not answer you. I am robot', author: 'robot', chatId: this.props.chatId})));
+        console.log(this.props.messagesChatsStore);
+    } 
+    
+
+    
+    render() {
+
+         
+
+        return <>
+            <h1>Number chat - {this.chatSelected()}</h1> 
+            <MessageList messages={this.props.messagesChatsStore.filter((e) => e.chatId === this.props.chatId)}/>
+            <SendMessage send={this.send} chatId={this.props.chatId}/>
+            </>
+    }
+
+
+};
+const mapStateToProps = store => ({  
+    messagesChatsStore: store.messagesChatsReducer
+});
+
+const mapDispatchToProps = dispath => ({
+    messagesChats: messagesChats
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatPage);
---
+++ src/components/Profile.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import {connect} from "react-redux";
+import {testText} from "./store/actions/testActions";
+
+
+class Profile extends React.Component {
+
+    state = {
+        authorMessage: ""
+    }
+
+    static defaultProps = {
+        textStore: "Error!"
+    }
+
+    inputValueName = (e) => {
+        this.setState({authorMessage: e.target.value});      
+    }
+
+    addAuthorNameNessage = () => {
+        this.props.textStore.text = this.state.authorMessage;
+        this.setState({});
+    }
+
+
+
+
+   
+    render () {
+        return <div>
+            <h1 style={{width: "100%", display: "block", margin: "40%", fontSize: "40px"}}>{this.props.textStore.text}</h1>
+            <input type="text" onChange={this.inputValueName}/>
+            <button onClick={this.addAuthorNameNessage}>Enter your name</button>
+        </div>
+    }
+};
+
+const mapStateToProps = store => ({
+    textStore: store.textReducer
+});
+
+const mapDispatchToProps = {
+    testText: testText
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
